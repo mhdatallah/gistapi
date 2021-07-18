c1be2c46c201c55d08ce8a28112ec937b9fd428d
Mohammad Atallah <m.atallah@beno.com>
2021-07-18 16:01:32 +0400

    Display owner name and info

--- src/components/Gist/Gist.js
+++ src/components/Gist/Gist.js
@@ -1,7 +1,18 @@
 import "./Gist.css"
 const Gist = ({ gist }) => {
     return (<>
-        <img src={gist.owner.avatar_url} alt="avatar"></img>
+        <div className="header">
+            <div className="owner">
+                <img src={gist.owner.avatar_url} alt="avatar"></img>
+                <a href={gist.owner.html_url}>{gist.owner.login}</a>
+            </div>
+            <div className="info">
+                <a href="javascript:void(0)">{Object.keys(gist.files).length} Files</a>
+                <a href={gist.forks_url}>Forks</a>
+                <a href={gist.comments_url}>Comments</a>
+                <a href={gist.owner.starred_url}>Stars</a>
+            </div>
+        </div>
     </>);
 };
 
