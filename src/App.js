bb7cf65e65c1bd4e91d42f4e5dda0d66397dfb93
Mohammad Atallah <m.atallah@beno.com>
2021-07-18 14:57:24 +0400

    Set up GistList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components'
 import Header from "./components/Header";
+import GistList from "./components/GistList";
 import GlobalStyles from "./GlobalStyle";
+import { getPublicGists } from "./services/gistService";
 
 const App = () => {
+  const [gists, setGists] = useState([]);
+
+  const fetchPublicGists = () => {
+    getPublicGists().then(res => {
+      setGists(res.data);
+    });
+  }
+
+  useEffect(() => {
+    fetchPublicGists();
+  }, []);
+
   return (
     <Wrapper className="App" data-testid="app">
       <Header />
       <GlobalStyles />
+      <GistList gists={gists} />
     </Wrapper>
   );
 }
--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,10 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React from 'react';
+import Gist from "./Gist";
+const GistList = ({ gists }) => {
+
+    const gistItems = gists.map((gist, i) => <Gist key={i} gist={gist} />);
+
+    return (<>{gistItems}</>);
+}
 
 export default GistList
